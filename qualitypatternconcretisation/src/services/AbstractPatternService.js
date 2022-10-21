1b8d6618f797a53780811a1adcbb97f9298aacd6
kesperar <arno.kesper@uni-marburg.de>
2022-10-21 17:38:19 +0200

    Update to a Vue 2022 Project.

---
+++ qualitypatternconcretisation/src/services/AbstractPatternService.js
@@ -0,0 +1,74 @@
+import api from "./axios";
+import axios from "axios"
+
+class AbstractPatternService {
+  baseUrl;
+
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  async getAbstractPatterns() {
+    try {
+      const payload = await axios.get(this.baseUrl);
+
+      return {
+        success: true,
+        data: payload.data,
+      };
+    } catch (err) {
+      return {
+        success: false,
+        data: err,
+      };
+    }
+  }
+
+  async getAbstractPatternTexts(abstractPattern) {
+    try {
+      const payload = await api.get(
+        this.baseUrl + `/text-list/${abstractPattern}`
+      );
+
+      return {
+        success: true,
+        data: payload.data,
+      };
+    } catch (err) {
+      return {
+        success: false,
+        data: err,
+      };
+    }
+  }
+
+  async postConcretePattern(body) {
+    try {
+      const payload = await api.post(
+        this.baseUrl +
+          `/instantiation/` +
+          `${body.abstractPattern}` +
+          `/` +
+          `${body.abstractPatternText}` +
+          `?name=` +
+          `${body.concretePatternName}`,
+        {
+          Headers: { Origin: "*" },
+        }
+      );
+      return {
+        success: true,
+        data: payload.data,
+      };
+    } catch (err) {
+      return {
+        success: false,
+        data: err,
+      };
+    }
+  }
+}
+
+export default new AbstractPatternService(
+  "http://localhost:8081/qualitypatternmodel/abstract-patterns"
+);
